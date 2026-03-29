f29f0a6934e264a689af33443adc63692c521b86
Mehmetkerem <hakankerem8@gmail.com>
2026-03-29 13:42:14 +0300

    fix: CategoryRepository List import eksikliği düzeltildi

--- e-commerce/src/main/java/com/mehmetkerem/repository/CategoryRepository.java
+++ e-commerce/src/main/java/com/mehmetkerem/repository/CategoryRepository.java
@@ -4,6 +4,8 @@ import com.mehmetkerem.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
     boolean existsByName(String name);
